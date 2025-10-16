11f6d8f31babe13c4b980c1898e2cee0df3032ce
Solange Casares <solangecasares@hotmail.com>
2025-10-16 18:11:56 -0300

    La pre-entrega - login 2

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -3,10 +3,10 @@ import sys
 import os
 from selenium.webdriver.common.by import By
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils.driver import get_driver
 from utils.helpers import entrar_a_la_pagina
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 URL = 'https://www.saucedemo.com/'
 USERNAME = 'standard_user'
@@ -17,6 +17,13 @@ def driver():
     driver = get_driver()
     yield driver
     driver.quit()
+
+def test_login(driver):
+    login_saucedemo(driver)
+
+    # Verifica que estamos en el inventario
+    assert '/inventory.html' in driver.current_url, "No se redirigió a la página de inventario después del login"
+    print('Login completado correctamente y se ingresó a la página de inventario.')
 
 def login_saucedemo(driver):
     entrar_a_la_pagina(driver, URL, 'Swag Labs')
@@ -32,10 +39,3 @@ def login_saucedemo(driver):
     driver.find_element(By.ID, "login-button").click()
 
     print("Se completaron correctamente los campos de login y se hizo clic en el botón.")
-
-def test_login():
-    login_saucedemo(driver)
-
-    # Verifica que estamos en el inventario
-    assert '/inventory.html' in driver.current_url, "No se redirigió a la página de inventario después del login"
-    print('Login completado correctamente y se ingresó a la página de inventario.')
